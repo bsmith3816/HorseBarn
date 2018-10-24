e7f9e13742559c619e25226bfc2224b6666a7675
BT_1N3_34 <bsmith3816@bths.edu>
2018-10-24 12:26:07 -0400

    10.24

---
+++ src/com/company/HorseBarn.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class HorseBarn {
+    private String name;
+    private int weight;
+    private Horse[] spaces;
+
+}
--- src/com/company/HorseRunner.java
+++ src/com/company/HorseRunner.java
@@ -4,16 +4,16 @@ public class HorseRunner {
 
     public static void main(String[] args) {
 	// write your code here
-        Horse horse1 = new Mustang("Secretariat", 12000);
-        Horse horse2 = new Mustang("Dusty Trail", 22000);
-        Horse horse3 = new Mustang("Silver", 12000);
+        HorseBarn horse1 = new Mustang("Secretariat", 12000);
+        HorseBarn horse2 = new Mustang("Dusty Trail", 22000);
+        HorseBarn horse3 = new Mustang("Silver", 12000);
 
-        Horse horses = new Horse[6];
+        HorseBarn horses = new HorseBarn[6];
 
         horses[1] = horse1;
         horses[3] = horse2;
         horses[5] = horse3;
-        HorseBarn barn = new HorseBarn(horses);
+        HorseRunner barn = new HorseRunner(horses);
 
         System.out.println(barn);
         System.out.println(" Dusty Trail is in space: ") + barn.findHorseSpace("Dusty Trail");
