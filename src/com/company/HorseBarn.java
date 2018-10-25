9c3a09afc3949a3cfd9c010b0512858992e049e3
BT_1N3_34 <bsmith3816@bths.edu>
2018-10-25 09:33:14 -0400

    10/25

--- src/com/company/HorseBarn.java
+++ src/com/company/HorseBarn.java
@@ -1,8 +1,27 @@
 package com.company;
 
 public class HorseBarn {
-    private String name;
-    private int weight;
     private Horse[] spaces;
-
+    public HorseBarn(Horse[] spaces){
+        this.spaces = spaces;
+    }
+    public int findHorseSpace(String name){
+        for(int i = 0;i < this.spaces.length; i++){
+            if(this.spaces[i] != null && name.equals(this.spaces[i].getName())){
+                return i;
+            }
+        }
+        return -1;
+    }
+    public void consolidate(){
+        int j = 0;
+        Horse[] newSpace = new Horse[this.spaces.length];
+        for(int i = 0; i < newSpace.length; i++){
+            if(this.spaces[i] != null){
+                newSpace[j] = this.spaces[i];
+                j++;
+            }
+        }
+        this.spaces = newSpace;
+    }
 }
--- src/com/company/HorseRunner.java
+++ src/com/company/HorseRunner.java
@@ -4,24 +4,26 @@ public class HorseRunner {
 
     public static void main(String[] args) {
 	// write your code here
-        HorseBarn horse1 = new Mustang("Secretariat", 12000);
-        HorseBarn horse2 = new Mustang("Dusty Trail", 22000);
-        HorseBarn horse3 = new Mustang("Silver", 12000);
+        Horse horse1 = new Mustang("Secretariat", 12000);
+        Horse horse2 = new Mustang("Dusty Trail", 22000);
+        Horse horse3 = new Mustang("Silver", 12000);
 
-        HorseBarn horses = new HorseBarn[6];
+        Horse[] horses = new Horse[6];
 
         horses[1] = horse1;
         horses[3] = horse2;
         horses[5] = horse3;
-        HorseRunner barn = new HorseRunner(horses);
+        HorseBarn barn = new HorseBarn(horses);
 
         System.out.println(barn);
-        System.out.println(" Dusty Trail is in space: ") + barn.findHorseSpace("Dusty Trail");
+        System.out.println(" Dusty Trail is in space: " + barn.findHorseSpace("Dusty Trail"));
 
         barn.consolidate();
 
         System.out.println(barn);
-        System.out.println(" Dusty Trail is now in space: ") + barn.findHorseSpace("Dusty Trail");
+        System.out.println(" Dusty Trail is now in space: " + barn.findHorseSpace("Dusty Trail"));
+
+        //System.out.println(" Dusty Trail is now in space: " + barn.findHorseSpace("Dusty Trail"));
 
     }
 }
